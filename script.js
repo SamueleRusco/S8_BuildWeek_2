ce04bc730e0c44138f68beb730aafd8a6c85db99
SamueleRusco <119084990+SamueleRusco@users.noreply.github.com>
2023-02-01 16:56:02 +0100

    some changes

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const getAlbum = async () => {
+// const getAlbum = async () => {
+//   try {
+//     const res = await fetch(
+//       "https://striveschool-api.herokuapp.com/api/deezer/album/75621062"
+//     );
+//     if (res.ok) {
+//       let data = await res.json();
+//       const array = Object.values(data);
+//       const jarray = Object.values(array[0]);
+//     }
+//   } catch (err) {
+//     console.log(err, "c'è un errore");
+//   }
+// };
+
+// getAlbum();
+
+getAlbum = async () => {
   try {
     const res = await fetch(
       "https://striveschool-api.herokuapp.com/api/deezer/album/75621062"
@@ -7,10 +24,12 @@ const getAlbum = async () => {
       let data = await res.json();
       const array = Object.values(data);
       const jarray = Object.values(array[0]);
+      let songs = data.tracks.data;
+
+      // console.log(songs);
+      songs.forEach((song) => console.log(song.preview));
     }
   } catch (err) {
     console.log(err, "c'è un errore");
   }
 };
-
-getAlbum();
