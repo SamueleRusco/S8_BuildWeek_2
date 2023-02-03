39835f2e019e88d93078af5924f0874879ae6a4a
leonardoantonelli <antonellileonardodesign@gmail.com>
2023-02-03 12:38:50 +0100

    ad responsive search page

--- scriptSearch.js
+++ scriptSearch.js
@@ -16,16 +16,19 @@ asyncWait = async function () {
         for (let i = 0; i < 10; i++) {
           let element = jarray[i];
           div.innerHTML += `         
-                        <div class="searchCard card d-flex align-items-center m-1" style="max-width: 15rem;">
+          <div class="col d-flex">
+                        <div class="searchCard card d-flex align-items-center w-100 h-100 " >
                         <img src="${element.album.cover_xl}" class="searchImgCard card-img-top" alt="">
-                        <div class="card-body">
+                        <div class="card-body d-flex flex-column">
                         <div class="textCardSearch" id="${element.artist.id}">${element.artist.name}</div>
-                        <div class="card-title">${element.title_short}</div>
+                        <div class="card-title ">${element.title_short}</div>
                         <div class="textCardSearch2">${element.album.title}</div>
                         
                         </div>
                         </div>
-                        </div>                        
+                        </div>
+                        </div> 
+
                         `;
         }
       } catch (err) {
